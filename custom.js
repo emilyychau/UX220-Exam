c904b90e95b4c57a0f79e3d15261119cdcf327d7
emilyychau <emilychau.yvo@gmail.com>
2023-12-20 16:06:14 -0500

    add text

--- custom.js
+++ custom.js
@@ -10,3 +10,20 @@ customElements.define("x-fullyear", YearElement);
 
 // Magic 8 Ball
 
+class Magic8Ball extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = 
+`
+<button id="question">Ask</button>
+<p id="answer"></p>
+`
+        this.button = document.querySelector("#question");
+        this.button.addEventListener("click", ()=>{
+            const answer = document.querySelector("#answer");
+            const aAnswers=["yes", "definitely", "no"];
+            const nAnswer = Math.floor(Math.random() * aAnswers.length);
+            answer.innerHTML = aAnswers[nAnswer];
+        })
+    }
+}
+customElements.define("x-magic", Magic8Ball);
